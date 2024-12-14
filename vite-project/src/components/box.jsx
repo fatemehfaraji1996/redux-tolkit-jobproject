56947aada8b526674dd5ed972ec992ae14724065
fatemehfaraji1996 <farajifateme1996@gmail.com>
2024-12-14 13:07:58 +0330

    commit

--- vite-project/src/components/box.jsx
+++ vite-project/src/components/box.jsx
@@ -1,24 +1,60 @@
-import React from "react";
 
-// import data from "../../Data/data.json";
+import React from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { selectSkill, clearSkill, setPersons } from '../redux/skilsSlice';
 import data from "../Data/data.json";
 
 export default function Box() {
+  const dispatch = useDispatch();
+  const selectedSkill = useSelector((state) => state.skills.selectedSkill);
+  const persons = useSelector((state) => state.skills.persons);
+
+  // بارگذاری داده‌ها در ریداکس
+  React.useEffect(() => {
+    dispatch(setPersons(data));
+  }, [dispatch]);
+
+  // هندلر برای انتخاب اسکیل
+  const handleSkillClick = (skill) => {
+    dispatch(selectSkill(skill));
+  };
+
+  // هندلر برای کلیر کردن اسکیل انتخاب شده
+  const handleClearClick = () => {
+    dispatch(clearSkill());
+  };
+
   return (
     <>
-      {/* {console.log(data)} */}
+      {selectedSkill && (
+        <div className="skill-box">
+          <button className='skill-box-btn' onClick={handleClearClick}>Clear</button>
+          {persons
+            .filter((person) => {
+              // بررسی شامل بودن اسکیل انتخاب شده در میان languages، tools، role و level
+              const allSkills = [...person.languages, ...person.tools, person.role, person.level];
+              return allSkills.includes(selectedSkill) && "pp"
+            })
+            .map((person) => (
+              <div key={person.id}>{person.name}</div>
+            ))}
+        </div>
+      )}
 
       <div className="bg-light">
-        {data.map((x, i) => {
-          return (
-            <div className="box">
+        {persons
+          .filter((person) => {
+            // نمایش فقط باکس‌های دارای اسکیل انتخاب شده
+            const allSkills = [...person.languages, ...person.tools, person.role, person.level];
+            return selectedSkill ? allSkills.includes(selectedSkill) : true;
+          })
+          .map((x, i) => (
+            <div key={i} className="box">
               <div className="pictureStyle">
                 <img src={x.logo} alt="" />
               </div>
-              {/* BTN LIGHT */}
               <div className="chaildBox1">
                 <div className="ppppppppp">
-                  {" "}
                   <p className="p-style">{x.company}</p>
                   <div className="lightBtnp">
                     <button className="lightBtn">
@@ -31,14 +67,10 @@ export default function Box() {
                     </button>
                   </div>
                 </div>
-
-                {/*  BTN DARK*/}
-                {/* POSITION P */}
                 <div className="coulemn">
                   <div className="positionStyle">
                     <p>{x.position}</p>
                   </div>
-                  {/* THREE P */}
                   <div className="boxthreep">
                     <p>{x.postedAt}</p>
                     <p>{x.contract}</p>
@@ -47,34 +79,25 @@ export default function Box() {
                 </div>
               </div>
               <div className="rightBax">
-                <button className="skilsBtn">
-                  <p className="skilsp">
-                    {x.role}
-                  </p>
+                <button className="skilsBtn" onClick={() => handleSkillClick(x.role)}>
+                  <p className="skilsp">{x.role}</p>
                 </button>
-                <button className="skilsBtn">
+                <button className="skilsBtn" onClick={() => handleSkillClick(x.level)}>
                   <p className="skilsp">{x.level}</p>
                 </button>
-                <button className="skilsBtn">
-                  <p className="skilsp">{x.languages[0]}</p>
-                </button>
-                <button className="skilsBtn">
-                  <p className="skilsp">{x.languages[1]}</p>
-                </button>
-                <button className="skilsBtn">
-                  <p className="skilsp">{x.languages[2]}</p>
-                </button>
-                <button className="skilsBtn">
-                  <p className="skilsp">{x.tools[0]}</p>
-                </button>
-                <button className="skilsBtn">
-                  <p className="skilsp">{x.tools[1]}</p>
-                  
-                </button>
+                {x.languages.map((lang, idx) => (
+                  <button key={idx} className="skilsBtn" onClick={() => handleSkillClick(lang)}>
+                    <p className="skilsp">{lang}</p>
+                  </button>
+                ))}
+                {x.tools.map((tool, idx) => (
+                  <button key={idx} className="skilsBtn" onClick={() => handleSkillClick(tool)}>
+                    <p className="skilsp">{tool}</p>
+                  </button>
+                ))}
               </div>
             </div>
-          );
-        })}
+          ))}
       </div>
     </>
   );
