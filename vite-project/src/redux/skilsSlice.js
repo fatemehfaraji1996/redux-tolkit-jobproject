c30c15eed909473cb57c66a6831b1e93e3859dd9
fatemehfaraji1996 <farajifateme1996@gmail.com>
2025-01-13 16:01:49 +0330

    commit

--- vite-project/src/redux/skilsSlice.js
+++ vite-project/src/redux/skilsSlice.js
@@ -1,44 +1,4 @@
-// import { createSlice } from '@reduxjs/toolkit'
-// const skilsSlice =createSlice({
-//     name:'skils',
-//     initialState:[],
-//     reducers:{
-//     create:((state,action)=>{
-//         state.push(action.payload)
-//     })
-//     }
-// })
-// export const  {create}= skilsSlice.actions
-// export default skilsSlice.reducer
-
-// import { createSlice } from '@reduxjs/toolkit';
-
-// const initialState = {
-//   selectedSkill: null,
-//   persons: [], // داده‌های اشخاص به‌صورت آرایه‌ای از اشیاء
-// };
-
-// const skillsSlice = createSlice({
-//   name: 'skills',
-//   initialState,
-//   reducers: {
-//     selectSkill(state, action) {
-//       state.selectedSkill = action.payload;
-//     },
-//     clearSkill(state) {
-//       state.selectedSkill = null;
-//     },
-//     setPersons(state, action) {
-//       state.persons = action.payload;
-//     },
-//   },
-// });
-
-// export const { selectSkill, clearSkill, setPersons } = skillsSlice.actions;
-// export default skillsSlice.reducer;
-
 import { createSlice } from '@reduxjs/toolkit';
-
 const initialState = {
   selectedSkills: [], // لیستی از اسکیل‌های انتخاب‌شده
   persons: [], // داده‌های اشخاص به‌صورت آرایه‌ای از اشیاء
